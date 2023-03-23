2da1f749f8642c75d4366d63fee6204766c65cc3
C-Obaze <cyprianobaze@gmail.com>
2023-03-23 11:24:31 +0100

    updated main.py

--- main.py
+++ main.py
@@ -31,16 +31,3 @@ model = smf.ols('Calorie_Burnage ~ Average_Pulse', health_data)
 results = model.fit()
 print(results.summary())
 plt.show()
-
-# A new line in our file!
-'''
-Summary - Predicting Calorie_Burnage with Average_Pulse
-- Coefficient of 0.3296, which means that Average_Pulse has a very small effect on Calorie_Burnage.
-- High P-value (0.824), which means that we cannot conclude a relationship between Average_Pulse and Calorie_Burnage.
-- R-Squared value of 0, which means that the linear regression function line does not fit the data well.
-- The 95% confidence interval for Average_Pulse is (-2.588, 3.247). This confidence interval does contain the 
-number “0”, which means that the true value for the coefficient of Average_Pulse could be zero,
-i.e. non-significant in predicting Calorie_Burnage.
-'''
-
-
